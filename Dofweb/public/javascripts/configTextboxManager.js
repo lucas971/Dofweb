76f79f93a326ce81fc4aa18e2ecc18b27506395c
Rhum <lucas.rioust@gmail.com>
2024-11-30 09:21:04 +0100

    on commence à s'en sortir

---
+++ Dofweb/public/javascripts/configTextboxManager.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.querySelector('textarea[name="message"]');
+
+    if (textarea) {
+        textarea.addEventListener('input', (e) => {
+            console.log('Typed:', e.target.value);
+        });
+    }
+    
+    console.log('test 1')
+});
