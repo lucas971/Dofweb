94f3d293dd511842d210e391c8547790adf39de3
Rhum <lucas.rioust@gmail.com>
2024-12-01 13:02:10 +0100

    favicon

--- Dofweb/app.js
+++ Dofweb/app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -45,6 +46,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('/favicon.png', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'favicon.png'));
 });
 
 module.exports = app;
